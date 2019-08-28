6a8d2011dff9cd694800b276c6a6d5ccd3ce5f07
ckinmon <42252221+ckinmon@users.noreply.github.com>
2019-08-27 20:45:10 -0400

    Add files via upload
    Updated Javadoc for the Game class.

--- source/Game.java
+++ source/Game.java
@@ -1,13 +1,32 @@
 import java.util.Scanner;
 
+/**
+ * @author Caleb Kinmon
+ * 
+ * This class creates the actual game logic and is used to execute a
+ * series of games of Rock, Paper, Scissors, Lizard, Spock. This class
+ * uses the following classes: Talker, Player, Outcome, and GameData.
+ * 
+ * A test run of the game can be found in the public main static method
+ * found at the end of this document.  
+ */
 public class Game{
 
+    /**
+     * This is the default constructor. Two parameters are required because
+     * the game requires two players. 
+     * @param p1 the first player of type Super class Player
+     * @param p2 the second player of type Super class Player
+     */
     Game(Player p1, Player p2){
         player1 = p1;
         player2 = p2;
         gamesToPlay = 10;
     }
 
+    /**
+     * This method initializes the game and displays the rules to the user.
+     */
     public void startGame(){
         talker = new Talker();
         outcome = new Outcome();
@@ -16,6 +35,9 @@ public class Game{
         talker.showRules();
     }
 
+    /**
+     * This method sets the number of games that the user wants to play.
+     */
     public void setGamesToPlay(){
         Scanner scanner = new Scanner(System.in);
         talker.askForNumberOfGames();
@@ -23,6 +45,10 @@ public class Game{
         scanner.close();
     }
 
+    /**
+     * This method executes the game and continuously plays the number of games set by 
+     * the user.
+     */
     public void play(){
         int count = 0;
         while(count++ < this.gamesToPlay){
@@ -43,10 +69,13 @@ public class Game{
     private Outcome outcome;
     private GameData data;
 
+    // This is an example test run of the Game, using a Computer player
+    // and an AI player.
     public static void main(String[] args){
+
         Game game = new Game(new ComputerPlayer(), new AIPlayer());
-        game.startGame();
-        game.setGamesToPlay();
+        game.startGame();  
+        game.setGamesToPlay(); 
         game.play();
     }
 
